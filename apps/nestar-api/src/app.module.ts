e98e5c86770d6a9faddc41e42161695965912408
Ali <mitali777770@gmail.com>
2026-08-12 10:03:09 +0900

    feat: build project standards

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,10 +5,12 @@ import { ConfigModule } from "@nestjs/config"
 import { GraphQLModule } from "@nestjs/graphql"
 import { ApolloDriver } from "@nestjs/apollo"
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 @Module({
   imports: [
     //--> env variablelarni oqish imkoniyatini beradi
-    ConfigModule.forRoot(), GraphQLModule.forRoot({ driver: ApolloDriver, playground: true, uploads: false, autoSchemaFile: true })
+    ConfigModule.forRoot(), GraphQLModule.forRoot({ driver: ApolloDriver, playground: true, uploads: false, autoSchemaFile: true }), ComponentsModule, DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
