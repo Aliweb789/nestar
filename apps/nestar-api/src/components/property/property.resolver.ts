b62f56cc62158b51686f4bf1f0ee63b083ede2ea
Ali <mitali777770@gmail.com>
2026-08-31 09:23:16 +0900

    feat: develop removePropertyByAdmin GraphQL API

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -92,4 +92,15 @@ export class PropertyResolver {
     input._id = shapeIntoMongoObjectId(input._id);
     return this.propertyService.updatePropertyByAdmin(input);
   }
+
+  @Roles(MemberType.ADMIN)
+  @UseGuards(RolesGuard)
+  @Mutation(() => Property)
+  public async removePropertyByAdmin(
+    @Args('propertyId') input: string,
+  ): Promise<Property> {
+    console.log('Mutation: removePropertyByAdmin');
+    const propertyId = shapeIntoMongoObjectId(input);
+    return this.propertyService.removePropertyByAdmin(propertyId);
+  }
 }
